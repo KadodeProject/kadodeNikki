e2de5cf6d378874a7825e278192c3e350d747ce1
Naofumi <63891531+Usuyuki@users.noreply.github.com>
2021-01-01 11:23:17 +0900

    feat マイグレーション準備

--- backend/database/migrations/2014_10_12_000000_create_users_table.php
+++ backend/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,11 +14,27 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('explain')->nullable()->comment('自己紹介');
+            $table->string('social_token')->nullable()->comment('ソーシャルのトークン');
+            $table->string('drive_token')->nullable()->comment('Googleドライブ系');
+
+            $table->dateTime('drive_backup_date')->nullable()->comment('Googleドライブバックアップ日');
+            $table->dateTime('imported')->nullable()->comment('インポート日');
+            $table->dateTime('exported')->nullable()->comment('エクスポート日');
+
+            $table->string('email')->nullable()->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable();
+
+            $table->string('profile_photo_path')->nullable()->comment('ソーシャルアイコンのURL');
+            $table->string('original_profile_photo_path')->nullable()->comment('オリジナルアイコン用のパス置き場');
+
+            $table->unsignedInteger('set1')->nullable()->comment('設定1');
+            $table->unsignedInteger('set2')->nullable()->comment('設定1');
+            
+            $table->unsignedInteger('premium')->nullable()->comment('プレミアム会員かどうか');
             $table->rememberToken();
             $table->timestamps();
         });

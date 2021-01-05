f53b610f7cc7e29bc4ff9a30cc38a7f85611cd3b
Naofumi <63891531+Usuyuki@users.noreply.github.com>
2021-01-05 10:11:39 +0900

    wip store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import ReactDOM from "react-dom";
+import "./index.css";
+
+import registerServiceWorker from "./registerServiceWorker";
+
+//redux
+import { createStore } from "redux";
+import { Provider } from "react-redux"; //作製したsotreを全コンポーネントに渡すコンポーネント
+import reducer from "./reducers";
+import App from "./components/App";
+
+//全階層に渡せる　全てのstateがこのstoreに集約
+//providerでどのコンポーネントでも使えうようにする→既存のコンポーネントをproviderコンポーネントでラップしてstoreという属性にするだけ
+//データのバケツリレー防止
+
+const store = createStore(reducer);
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();

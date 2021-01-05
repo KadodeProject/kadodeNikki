19818abcb443c213379cd278c78a22aa6000aeb6
Naofumi <63891531+Usuyuki@users.noreply.github.com>
2021-01-05 09:32:06 +0900

    wip component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,17 +11,60 @@ import React, { Component } from "react";
 //   }
 // }
 
-//関数コンポーネント
+//propsはcomponentの属性→props.nameなどで属性に対して参照できる（数値、文字列、関数など）
+
 const App = () => {
+  const profiles = [
+    {
+      name: "Taro",
+      age: 10,
+    },
+    {
+      name: "Hanako",
+      age: 5,
+    },
+    {
+      name: "AA",
+    },
+  ];
   return (
     <div>
-      <Cat />
-      <Cat />
-      <Cat />
+      {profiles.map((profile, index) => {
+        return <User name={profiles.name} age={profile.age} key={index} />;
+        // ユニークなkeyが必要→mapのindex機能を利用する
+      })}
+      {/* <User name={"Taro"} age={10} />
+      <User name={"Hanako"} age={5} /> */}
+      {/* ↑ユーザーコンポーネントに対してnameというプロップス属性を与えている */}
     </div>
   );
 };
-const Cat = () => {
-  return <div>Meow!</div>;
+
+// Appで受け取ったpropsを読む
+const User = (props) => {
+  return (
+    <div>
+      Hi,I'm {props.name}, and {props.age} years old!
+    </div>
+  );
 };
+
+//デフォルトprops
+User.defaultProps = {
+  age: 1,
+};
+
+//関数コンポーネント
+// const App = () => {
+//   return (
+//     <div>
+//       <Cat />
+//       <Cat />
+//       <Cat />
+//     </div>
+//   );
+// };
+// const Cat = () => {
+//   return <div>Meow!</div>;
+// };
 export default App;
